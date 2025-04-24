e56741abb890b5dc60f8d5a89a4fc9fa5b3640e9
Zheng Ma <mazhengcn@outlook.com>
2025-04-24 17:06:19 +0800

    feat: Update cookiecutter template to improve project structure and documentation fix: Correct author name references in various files chore: Remove unnecessary test file and clean up project settings

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,31 +1,10 @@
-#!/usr/bin/env python
-
-import subprocess
 import pathlib
+import subprocess
 
 
-def init_git_repo():
-    """
-    Initialize a git repository and make an initial commit.
-    """
-    # Initialize a new git repository
-    subprocess.call(["git", "init", "--initial-branch=main"])
-    # Add all files to the staging area
-    subprocess.call(["git", "add", "*"])
-    # Make an initial commit
-    subprocess.call(["git", "commit", "-m", "Initial commit"])
+if "Not open source" == "{{ cookiecutter.open_source_license }}":
+    pathlib.Path("LICENSE").unlink()
 
-
-if __name__ == "__main__":
-
-    if '{{ cookiecutter.create_author_file }}' != 'y':
-        pathlib.Path('AUTHORS.rst').unlink()
-        pathlib.Path('docs', 'authors.rst').unlink()
-
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        pathlib.Path('src', '{{ cookiecutter.module_name }}', 'cli.py').unlink()
-
-    if 'Not open source' == '{{ cookiecutter.open_source_license }}':
-        pathlib.Path('LICENSE').unlink()
-
-    init_git_repo()
+subprocess.call(["git", "init", "--initial-branch=main"])
+subprocess.call(["git", "add", "*"])
+subprocess.call(["git", "commit", "-m", "Initial commit"])
--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.module_name }}/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for {{ cookiecutter.repo_name }}."""
 
-__author__ = """{{ cookiecutter.full_name }}"""
-__email__ = '{{ cookiecutter.email }}'
-__version__ = '{{ cookiecutter.project_version }}'
+__author__ = """{{ cookiecutter.author_name }}"""
+__email__ = "{{ cookiecutter.email }}"
+__version__ = "{{ cookiecutter.project_version }}"
